60eb7c1128b2fa338c99cfd8cd4f9e89db0a55bd
Fredrik Fornwall <fredrik@fornwall.net>
2024-05-21 12:26:30 +0200

    terminal-{emulator,view}: build.gradle -> build.gradle.kts

---
+++ terminal-emulator/build.gradle.kts
@@ -0,0 +1,50 @@
+plugins {
+    id("com.android.library")
+}
+
+android {
+    namespace = "com.termux.emulator"
+
+    val ndkVersion: String by project
+    this.ndkVersion = ndkVersion
+
+    defaultConfig {
+        val minSdkVersion: String by project
+        val compileSdkVersion: String by project
+        minSdk = minSdkVersion.toInt()
+        compileSdk = compileSdkVersion.toInt()
+
+        externalNativeBuild {
+            ndkBuild {
+                cFlags += listOf("-std=c11", "-Wall", "-Wextra", "-Werror", "-Os", "-fno-stack-protector", "-Wl,--gc-sections")
+            }
+        }
+
+        ndk {
+            abiFilters += listOf("x86", "x86_64", "armeabi-v7a", "arm64-v8a")
+        }
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+        }
+    }
+
+    externalNativeBuild {
+        ndkBuild {
+            path = File("src/main/jni/Android.mk")
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+dependencies {
+    implementation("androidx.annotation:annotation:1.7.1")
+    testImplementation("junit:junit:4.13.2")
+}
---
+++ terminal-view/build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+    id("com.android.library")
+}
+
+android {
+    namespace = "com.termux.view"
+
+    dependencies {
+        implementation("androidx.annotation:annotation:1.7.1")
+        api(project(":terminal-emulator"))
+    }
+
+    defaultConfig {
+        val minSdkVersion: String by project
+        val targetSdkVersion: String by project
+        val compileSdkVersion: String by project
+        minSdk = minSdkVersion.toInt()
+        targetSdk = targetSdkVersion.toInt()
+        compileSdk = compileSdkVersion.toInt()
+
+        testInstrumentationRunner = "android.support.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+dependencies {
+    testImplementation("junit:junit:4.13.2")
+}
