ef9f0683e0d9660c6f5b714ce37d4e8857e5a5cb
Fredrik Fornwall <fredrik@fornwall.net>
2024-05-26 22:42:41 +0200

    termux-tasker: build.gradle -> build.gradle.kts

---
+++ termux-tasker/build.gradle.kts
@@ -0,0 +1,67 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.termux.tasker"
+
+    defaultConfig {
+        versionCode = 7
+        versionName = "0.7.0"
+
+        val minSdkVersion: String by project
+        val targetSdkVersion: String by project
+        val compileSdkVersion: String by project
+        minSdk = minSdkVersion.toInt()
+        targetSdk = targetSdkVersion.toInt()
+        compileSdk = compileSdkVersion.toInt()
+
+        //manifestPlaceholders.TERMUX_PACKAGE_NAME = "com.termux"
+        //manifestPlaceholders.TERMUX_APP_NAME = "Termux"
+        //manifestPlaceholders.TERMUX_TASKER_APP_NAME = "Termux:Tasker"
+    }
+
+    signingConfigs {
+        getByName("debug") {
+            storeFile = file("testkey_untrusted.jks")
+            keyAlias = "alias"
+            storePassword = "xrj45yWGLbsO7W0v"
+            keyPassword = "xrj45yWGLbsO7W0v"
+        }
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+        }
+
+        getByName("debug") {
+            signingConfig = signingConfigs.getByName("debug")
+        }
+
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+dependencies {
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.annotation:annotation:1.8.0")
+    implementation("com.google.android.material:material:1.12.0")
+
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test:runner:1.5.2")
+    androidTestImplementation("androidx.test:rules:1.5.0")
+}
+
+task("versionName") {
+    doLast {
+        print(android.defaultConfig.versionName)
+    }
+}
