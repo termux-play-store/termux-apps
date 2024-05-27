4de32a5bc3a5a79ca5b8a10c0e99f2ddfb7dab4b
Fredrik Fornwall <fredrik@fornwall.net>
2024-05-27 12:28:24 +0200

    Extract the Termux ContentProvider to its own toplevel class

---
+++ termux-app/src/main/java/com/termux/app/TermuxContentProvider.java
@@ -0,0 +1,126 @@
+package com.termux.app;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.MatrixCursor;
+import android.net.Uri;
+import android.os.Environment;
+import android.os.ParcelFileDescriptor;
+import android.provider.MediaStore;
+import android.webkit.MimeTypeMap;
+
+import androidx.annotation.NonNull;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Locale;
+
+public class TermuxContentProvider extends ContentProvider {
+
+    @Override
+    public boolean onCreate() {
+        return true;
+    }
+
+    @Override
+    public Cursor query(@NonNull Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+        File file = new File(uri.getPath());
+
+        if (projection == null) {
+            projection = new String[]{
+                MediaStore.MediaColumns.DISPLAY_NAME,
+                MediaStore.MediaColumns.SIZE,
+                MediaStore.MediaColumns._ID
+            };
+        }
+
+        MatrixCursor cursor = new MatrixCursor(projection);
+        addFileRow(cursor, file, projection);
+        return cursor;
+    }
+
+    private void addFileRow(MatrixCursor cursor, File file, String[] projection) {
+        if (file.isDirectory()) {
+            var contentFiles = file.listFiles();
+            if (contentFiles != null) {
+                for (var contentFile : contentFiles) {
+                    addFileRow(cursor, contentFile, projection);
+                }
+            }
+            return;
+        }
+        Object[] row = new Object[projection.length];
+        for (int i = 0; i < projection.length; i++) {
+            String column = projection[i];
+            Object value;
+            switch (column) {
+                case MediaStore.MediaColumns.DISPLAY_NAME:
+                    value = file.getName();
+                    break;
+                case MediaStore.MediaColumns.RELATIVE_PATH:
+                    // TODO: Test and validate
+                    value = file.getParentFile().getAbsolutePath().substring(TermuxConstants.FILES_PATH.length()) + "/";
+                    break;
+                case MediaStore.MediaColumns.SIZE:
+                    value = (int) file.length();
+                    break;
+                case MediaStore.MediaColumns._ID:
+                    value = 1;
+                    break;
+                default:
+                    value = null;
+            }
+            row[i] = value;
+        }
+        cursor.addRow(row);
+    }
+
+    @Override
+    public String getType(@NonNull Uri uri) {
+        String path = uri.getLastPathSegment();
+        if (path != null) {
+            int extIndex = path.lastIndexOf('.') + 1;
+            if (extIndex > 0) {
+                MimeTypeMap mimeMap = MimeTypeMap.getSingleton();
+                String ext = path.substring(extIndex).toLowerCase(Locale.ROOT);
+                return mimeMap.getMimeTypeFromExtension(ext);
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public Uri insert(@NonNull Uri uri, ContentValues values) {
+        return null;
+    }
+
+    @Override
+    public int delete(@NonNull Uri uri, String selection, String[] selectionArgs) {
+        return 0;
+    }
+
+    @Override
+    public int update(@NonNull Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+        return 0;
+    }
+
+    @Override
+    public ParcelFileDescriptor openFile(@NonNull Uri uri, @NonNull String mode) throws FileNotFoundException {
+        File file = new File(uri.getPath());
+        try {
+            String path = file.getCanonicalPath();
+            // String callingPackageName = getCallingPackage();
+            String storagePath = Environment.getExternalStorageDirectory().getCanonicalPath();
+            // See https://support.google.com/faqs/answer/7496913:
+            if (!(path.startsWith(TermuxConstants.FILES_PATH) || path.startsWith(storagePath))) {
+                throw new IllegalArgumentException("Invalid path: " + path);
+            }
+        } catch (IOException e) {
+            throw new IllegalArgumentException(e);
+        }
+
+        return ParcelFileDescriptor.open(file, ParcelFileDescriptor.parseMode(mode));
+    }
+}
--- termux-app/src/main/java/com/termux/app/TermuxOpenReceiver.java
+++ termux-app/src/main/java/com/termux/app/TermuxOpenReceiver.java
@@ -2,24 +2,14 @@ package com.termux.app;
 
 import android.content.ActivityNotFoundException;
 import android.content.BroadcastReceiver;
-import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
-import android.database.Cursor;
-import android.database.MatrixCursor;
 import android.net.Uri;
-import android.os.Environment;
-import android.os.ParcelFileDescriptor;
-import android.provider.MediaStore;
 import android.util.Log;
 import android.webkit.MimeTypeMap;
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.util.Locale;
-
-import androidx.annotation.NonNull;
 
 public class TermuxOpenReceiver extends BroadcastReceiver {
 
@@ -114,116 +104,6 @@ public class TermuxOpenReceiver extends BroadcastReceiver {
             context.startActivity(sendIntent);
         } catch (ActivityNotFoundException e) {
             Log.e(LOG_TAG, "No app handles the url " + data);
-        }
-    }
-
-    public static class ContentProvider extends android.content.ContentProvider {
-
-        private static final String LOG_TAG = "TermuxContentProvider";
-
-        @Override
-        public boolean onCreate() {
-            return true;
-        }
-
-        @Override
-        public Cursor query(@NonNull Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
-            File file = new File(uri.getPath());
-
-            if (projection == null) {
-                projection = new String[]{
-                    MediaStore.MediaColumns.DISPLAY_NAME,
-                    MediaStore.MediaColumns.SIZE,
-                    MediaStore.MediaColumns._ID
-                };
-            }
-
-            MatrixCursor cursor = new MatrixCursor(projection);
-            addFileRow(cursor, file, projection);
-            return cursor;
-        }
-
-        private void addFileRow(MatrixCursor cursor, File file, String[] projection) {
-            if (file.isDirectory()) {
-                var contentFiles = file.listFiles();
-                if (contentFiles != null) {
-                    for (var contentFile : contentFiles) {
-                        addFileRow(cursor, contentFile, projection);
-                    }
-                }
-                return;
-            }
-            Object[] row = new Object[projection.length];
-            for (int i = 0; i < projection.length; i++) {
-                String column = projection[i];
-                Object value;
-                switch (column) {
-                    case MediaStore.MediaColumns.DISPLAY_NAME:
-                        value = file.getName();
-                        break;
-                    case MediaStore.MediaColumns.RELATIVE_PATH:
-                        // TODO: Test and validate
-                        value = file.getParentFile().getAbsolutePath().substring(TermuxConstants.FILES_PATH.length()) + "/";
-                        break;
-                    case MediaStore.MediaColumns.SIZE:
-                        value = (int) file.length();
-                        break;
-                    case MediaStore.MediaColumns._ID:
-                        value = 1;
-                        break;
-                    default:
-                        value = null;
-                }
-                row[i] = value;
-            }
-            cursor.addRow(row);
-        }
-
-        @Override
-        public String getType(@NonNull Uri uri) {
-            String path = uri.getLastPathSegment();
-            if (path != null) {
-                int extIndex = path.lastIndexOf('.') + 1;
-                if (extIndex > 0) {
-                    MimeTypeMap mimeMap = MimeTypeMap.getSingleton();
-                    String ext = path.substring(extIndex).toLowerCase(Locale.ROOT);
-                    return mimeMap.getMimeTypeFromExtension(ext);
-                }
-            }
-            return null;
-        }
-
-        @Override
-        public Uri insert(@NonNull Uri uri, ContentValues values) {
-            return null;
-        }
-
-        @Override
-        public int delete(@NonNull Uri uri, String selection, String[] selectionArgs) {
-            return 0;
-        }
-
-        @Override
-        public int update(@NonNull Uri uri, ContentValues values, String selection, String[] selectionArgs) {
-            return 0;
-        }
-
-        @Override
-        public ParcelFileDescriptor openFile(@NonNull Uri uri, @NonNull String mode) throws FileNotFoundException {
-            File file = new File(uri.getPath());
-            try {
-                String path = file.getCanonicalPath();
-                // String callingPackageName = getCallingPackage();
-                String storagePath = Environment.getExternalStorageDirectory().getCanonicalPath();
-                // See https://support.google.com/faqs/answer/7496913:
-                if (!(path.startsWith(TermuxConstants.FILES_PATH) || path.startsWith(storagePath))) {
-                    throw new IllegalArgumentException("Invalid path: " + path);
-                }
-            } catch (IOException e) {
-                throw new IllegalArgumentException(e);
-            }
-
-            return ParcelFileDescriptor.open(file, ParcelFileDescriptor.parseMode(mode));
         }
     }
 
