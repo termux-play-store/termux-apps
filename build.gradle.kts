b4bd55a725103472ac5015c4eee6c61a4fa4cd53
Fredrik Fornwall <fredrik@fornwall.net>
2024-06-14 11:33:43 +0200

    Update the android gradle plug-in to 8.4.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ buildscript {
 
     dependencies {
         val kotlinVersion = rootProject.extra.get("kotlin_version") as String
-        classpath("com.android.tools.build:gradle:8.4.2")
+        classpath("com.android.tools.build:gradle:8.5.0")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
     }
 }
