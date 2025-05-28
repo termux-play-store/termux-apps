3863aa3c546f19d2267f4d913edc62672dd492ca
Fredrik Fornwall <fredrik@fornwall.net>
2025-05-28 17:13:14 +0200

    Bump androidx.core:core from 1.15.0 to 1.16.0

--- termux-app/build.gradle.kts
+++ termux-app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
 
     dependencies {
         implementation("androidx.annotation:annotation:1.9.1")
-        implementation("androidx.core:core:1.15.0")
+        implementation("androidx.core:core:1.16.0")
         implementation("androidx.drawerlayout:drawerlayout:1.2.0")
         implementation("androidx.viewpager:viewpager:1.1.0")
         implementation("com.google.android.material:material:1.12.0")
