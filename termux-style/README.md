8e21f3b3662562837894e0d774a9a7ec17275857
Fredrik Fornwall <fredrik@fornwall.net>
2026-02-11 22:51:18 +0100

    termux-style: Update fonts
    - Update nerd fonts from 3.3 to 3.4
    - Add Adwaita Mono font
    - Add Atkinson Hyperlegible Mono font
    - Add Noto font
    - Add Breeze color theme
    - Remove Bedstead Condensed font
    - Remove Courier Prime font
    - Remove GNU FreeFont font
    - Bump version to 0.35
    - Download fonts during the gradle build and gitignore them

--- termux-style/README.md
+++ termux-style/README.md
@@ -1,15 +1,10 @@
 # Termux:Style
 
-A [Termux](https://termux.org) add-on app to customize the terminal font and color theme.
+A [Termux](https://termux.com) add-on app to customize the terminal font and color theme.
 
 ## Installation
 
 The `Termux:Style` application can be obtained from [Google Play](https://play.google.com/store/apps/details?id=com.termux.styling).
-
-Additionally we provide per-commit debug builds for those who want to try
-out the latest features or test their pull request. This build can be obtained
-from one of the workflow runs listed on [Github Actions](https://github.com/termux/termux-styling/actions)
-page.
 
 ## How to use
 
--- termux-style/build.gradle.kts
+++ termux-style/build.gradle.kts
@@ -1,4 +1,8 @@
+import com.android.build.gradle.internal.tasks.factory.dependsOn
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import java.io.BufferedOutputStream
+import java.io.FileOutputStream
+import java.net.URI
 
 plugins {
     id("com.android.application")
@@ -9,7 +13,7 @@ android {
     namespace = "com.termux.styling"
 
     defaultConfig {
-        versionCode = 34
+        versionCode = 35
         versionName = "0.$versionCode"
 
         val minSdkVersion: String by project
@@ -34,11 +38,14 @@ android {
         }
     }
 
-     buildTypes {
-         getByName("release") {
+    buildTypes {
+        getByName("release") {
             isMinifyEnabled = true
             isShrinkResources = true
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.txt")
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.txt"
+            )
         }
 
         getByName("debug") {
@@ -71,5 +78,93 @@ dependencies {
 tasks.register("versionName") {
     doLast {
         print(android.defaultConfig.versionName)
+    }
+}
+
+
+fun downloadFile(downloadTo: File, remoteUrl: String) {
+    if (downloadTo.exists()) {
+        logger.info("Keeping local file: ${downloadTo.absolutePath}")
+        return;
+    }
+
+    logger.quiet("Downloading $remoteUrl to $downloadTo ...")
+    downloadTo.parentFile.mkdirs()
+    val tmpFile = File(downloadTo.absolutePath + ".tmp")
+    BufferedOutputStream(FileOutputStream(tmpFile)).use {
+        val connection = URI(remoteUrl).toURL().openConnection()
+        connection.inputStream.transferTo(it)
+    }
+    tmpFile.renameTo(downloadTo)
+}
+
+tasks {
+    getByName<Delete>("clean") {
+        doLast {
+            val tree = fileTree(File(projectDir, "src/main/assets/fonts"))
+            tree.include("**/*.otf")
+            tree.include("**/*.ttf")
+            tree.forEach { it.delete() }
+        }
+    }
+}
+
+tasks.register("downloadPrebuilt") {
+    doLast {
+        val nerdFontsVersion = "3.4.0"
+        val fonts = mapOf(
+            "Adwaita-Mono" to "AdwaitaMonoNerdFont-Regular.ttf",
+            "Anonymous-Pro" to "AnonymiceProNerdFont-Regular.ttf",
+            "Atkinson-Hyperlegible-Mono" to "AtkynsonMonoNerdFont-Regular.otf",
+            "Cascadia-Code" to "CaskaydiaCoveNerdFont-Regular.ttf",
+            "D2-Coding" to "D2CodingLigatureNerdFont-Regular.ttf",
+            "DejaVu-Sans-Mono" to "DejaVuSansMNerdFont-Regular.ttf",
+            "Fantasque-Sans-Mono" to "FantasqueSansMNerdFont-Regular.ttf",
+            "Fira-Code" to "FiraCodeNerdFont-Regular.ttf",
+            "Fira-Mono" to "FiraMonoNerdFont-Regular.otf",
+            "Go-Mono" to "GoMonoNerdFont-Regular.ttf",
+            "Hack" to "HackNerdFont-Regular.ttf",
+            "Hermit" to "HurmitNerdFont-Regular.otf",
+            "Inconsolata" to "InconsolataNerdFont-Regular.ttf",
+            "Iosevka" to "IosevkaNerdFont-Regular.ttf",
+            "JetBrains-Mono" to "JetBrainsMonoNerdFont-Regular.ttf",
+            "Liberation-Mono" to "LiterationMonoNerdFont-Regular.ttf",
+            "Meslo" to "MesloLGLNerdFont-Regular.ttf",
+            "Monofur" to "MonofurNerdFont-Regular.ttf",
+            "Monoid" to "MonoidNerdFont-Regular.ttf",
+            "Noto" to "NotoMonoNerdFont-Regular.ttf",
+            "OpenDyslexic" to "OpenDyslexicMNerdFont-Regular.otf",
+            "Roboto-Mono" to "RobotoMonoNerdFont-Regular.ttf",
+            "Source-Code-Pro" to "SauceCodeProNerdFont-Regular.ttf",
+            "Terminus" to "TerminessNerdFont-Regular.ttf",
+            "Ubuntu-Mono" to "UbuntuMonoNerdFont-Regular.ttf",
+            "Victor-Mono" to "VictorMonoNerdFont-Regular.ttf"
+        )
+        fonts.forEach { (fontName, fontFile) ->
+            val fontPack = if (fontName == "Go-Mono") fontName else fontName.replace("-", "")
+            val fontUrl =
+                "https://github.com/ryanoasis/nerd-fonts/releases/download/v${nerdFontsVersion}/${fontPack}.zip"
+            val cacheDir = File(layout.buildDirectory.asFile.get(), "termux-fonts")
+            val zipFile = File(cacheDir, "${fontPack}.zip")
+            downloadFile(zipFile, fontUrl)
+            val destinationDir = "src/main/assets/fonts/"
+            val destinationFileName = "${fontName}.ttf"
+            copy {
+                from(zipTree(zipFile)) {
+                    include(fontFile)
+                }
+                into(destinationDir)
+                eachFile {
+                    relativePath = RelativePath(true, destinationFileName)
+                }
+            }
+        }
+    }
+}
+
+afterEvaluate {
+    android.applicationVariants.all { variant ->
+        variant.javaCompileProvider.dependsOn("downloadPrebuilt")
+        true
     }
 }
